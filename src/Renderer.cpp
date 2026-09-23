d61c35b87f21dc07eb5f54a3a260a62563ef43a9
CharlesAcmen <15591542871@163.com>
2026-09-23 17:16:31 +0800

    poolSizes

--- src/Renderer.cpp
+++ src/Renderer.cpp
@@ -217,7 +217,7 @@ void Renderer::CreateDescriptorPool() {
     poolInfo.sType = VK_STRUCTURE_TYPE_DESCRIPTOR_POOL_CREATE_INFO;
     poolInfo.poolSizeCount = static_cast<uint32_t>(poolSizes.size());
     poolInfo.pPoolSizes = poolSizes.data();
-    poolInfo.maxSets = 5;
+    poolInfo.maxSets = 2 + modelCount + bladeGroupCount;
 
     if (vkCreateDescriptorPool(logicalDevice, &poolInfo, nullptr, &descriptorPool) != VK_SUCCESS) {
         throw std::runtime_error("Failed to create descriptor pool");
